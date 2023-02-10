e049d48c0df0328dfac6e46ae082ad5a73afcfa9
Arina Levchuk <aryna.liauchuk@awtg.co.uk>
2023-02-10 15:42:31 +0300

    initial project setup

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Arina Levchuk on 10.02.23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
